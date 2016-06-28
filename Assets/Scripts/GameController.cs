5989c1a7905cee5fe708f89d6d0e2ef02df6a93b
Ryan Nordman <spacerobots@gmail.com>
2016-06-28 13:36:07 -0700

    WASD controls to move the main camera around.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,4 +2,28 @@
 using System.Collections;
 
 public class GameController : MonoSingleton<GameController> {
+
+	public Camera mainCamera;
+	public float speedMultiplier = 0.5f;
+
+	public void Update() {
+		if (Input.GetKey ("w")) {
+			move (mainCamera.transform.forward, mainCamera.transform);
+		}
+		if (Input.GetKey ("s")) {
+			move (-mainCamera.transform.forward, mainCamera.transform);
+		}
+		if (Input.GetKey ("a")) {
+			move (-mainCamera.transform.right, mainCamera.transform);
+		}
+		if (Input.GetKey ("d")) {
+			move (mainCamera.transform.right, mainCamera.transform);
+		}
+	}
+
+	private void move(Vector3 direction, Transform entity) {
+		Vector3 pos = entity.position;
+		pos += direction * speedMultiplier;
+		entity.position = pos;
+	}
 }
