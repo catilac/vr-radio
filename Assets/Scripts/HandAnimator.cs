ce0f25dd5f65f0c9d18688559484f629b60cf508
Ryan Nordman <spacerobots@gmail.com>
2016-06-29 19:01:57 -0700

    Keyboard keys 'b' 'n' and 'm' trigger transitions between hand poses.

--- Assets/Scripts/HandAnimator.cs
+++ Assets/Scripts/HandAnimator.cs
@@ -3,9 +3,39 @@ using System.Collections;
 
 public class HandAnimator : MonoBehaviour {
 
-	public Animation anim;
+	private Animator anim;
 
 	public void Start() {
-		anim.Play ();
+		anim = GetComponent<Animator> ();
+	}
+	public bool idleHand;
+	public bool thumbHand;
+	public bool pointHand;
+
+	public void Update() {
+		if (Input.GetKeyDown ("b")) {
+			idleHand = true;
+			thumbHand = false;
+			pointHand = false;
+			updateParams ();
+		}
+		if (Input.GetKeyDown ("n")) {
+			idleHand = false;
+			thumbHand = true;
+			pointHand = false;
+			updateParams ();
+		}
+		if (Input.GetKeyDown ("m")) {
+			idleHand = false;
+			thumbHand = false;
+			pointHand = true;
+			updateParams ();
+		}
+	}
+
+	private void updateParams() {
+		anim.SetBool ("idleHand", idleHand);
+		anim.SetBool ("thumbHand", thumbHand);
+		anim.SetBool ("pointHand", pointHand);
 	}
 }
